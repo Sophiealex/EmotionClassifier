a32998a2ff9f7a570365538d1b1e0a18339196b8
Jacob Carse <jacob.carse@gmail.com>
2016-11-23 12:14:34 +0000

    Added Timer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import builddata
 import emotionclassifier
 
@@ -8,27 +9,35 @@ def main():
         mode = sys.argv[1]
         if mode == 'train':
             if len(sys.argv) > 4:
+                start = time.clock()
                 faces = builddata.get_data(sys.argv[2])
                 training_data, testing_data = emotionclassifier.divide_data(faces)
                 print 'number of training examples = ' + str(len(training_data))
                 print 'number of testing examples  = ' + str(len(testing_data)) + '\n'
                 classifier = emotionclassifier.EmotionClassifier(8, sys.argv[3])
                 classifier.train(training_data, testing_data, int(sys.argv[4]))
+                end = time.clock()
+                print 'Training Time: ' + str(end - start) + 's'
             else:
                 print 'Please add \'Image Dir\' \'Session Save Path\' \'Number of Epochs\''
 
         elif mode == 'classify':
             if len(sys.argv) > 3:
+                start = time.clock()
                 face = builddata.get_face(sys.argv[2])
                 classifier = emotionclassifier.EmotionClassifier(8, sys.argv[3])
                 classification = classifier.classify(face)
-                print sys.argv[2] + ' -> ' + str(classification[1])
+                end = time.clock()
+                print sys.argv[2] + ' classified as type ' + str(classification[1]) + ' in ' + str(end - start) + 's'
             else:
                 print 'Please add \'Image Path\' \'Session Save Path\''
 
         elif mode == 'build':
             if len(sys.argv) > 4:
+                start = time.clock()
                 builddata.build_data(sys.argv[2], sys.argv[3], sys.argv[4])
+                end = time.clock()
+                print 'Augmented Dataset built in ' + str(end - start) + 's'
             else:
                 'Please add \'Image Dir\' \'Label Dir\' \'Output Dir\''
 
