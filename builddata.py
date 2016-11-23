b188ef20c41162a668646d2c94dbba1c2e7bee4a
Jacob Carse <jacob.carse@gmail.com>
2016-11-23 18:46:56 +0000

    Fixed bug when reading Dataset Labels

--- builddata.py
+++ builddata.py
@@ -16,6 +16,7 @@ def build_data(image_dir, label_dir, output_dir, itype='png'):
     :type itype: str
     :return: void
     """
+    count = 0
     for folder in os.listdir(image_dir):
         if os.path.isdir(image_dir + folder):
             for inner_folder in os.listdir(image_dir + folder):
@@ -25,35 +26,38 @@ def build_data(image_dir, label_dir, output_dir, itype='png'):
                         if os.path.isfile(label_file):
                             f = open(label_file, 'r')
                             label = int(float(f.readline()))
-                            image_file = image_dir + folder + '/' + inner_folder + '/' + input_file
+                            for image_file in os.listdir(image_dir + folder + '/' + inner_folder):
+                                image_file = image_dir + folder + '/' + inner_folder + '/' + image_file
+                                clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+                                image = clahe.apply(cv2.imread(image_file, cv2.IMREAD_GRAYSCALE))
+                                detector = dlib.get_frontal_face_detector()
+                                detections = detector(image, 1)
+                                for _, d in enumerate(detections):
+                                    left, right, top, bottom = d.left()-20, d.right()+20, d.top()-20, d.bottom()+20
+                                    face = image[top:bottom, left:right]
+                                    face = cv2.resize(face, (96, 96))
+                                    patches = [face[0:88, 0:88], face[8:96, 0:88], face[0:88, 8:96],
+                                               face[8:96, 8:96], face[4:92, 4:92]]
+                                    for i in range(len(patches)):
+                                        cv2.imwrite(output_dir + str(label) + '/' + input_file[:-4] + str(i) + '.' +
+                                                    itype, patches[i])
+                                        print count
+                                        count += 1
 
-                            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-                            image = clahe.apply(cv2.imread(image_file, cv2.IMREAD_GRAYSCALE))
-                            detector = dlib.get_frontal_face_detector()
-                            detections = detector(image, 1)
-                            for _, d in enumerate(detections):
-                                left, right, top, bottom = d.left() - 20, d.right() + 20, d.top() - 20, d.bottom() + 20
-                                face = image[top:bottom, left:right]
-                                face = cv2.resize(face, (96, 96))
-                                patches = [face[0:88, 0:88], face[8:96, 0:88], face[0:88, 8:96],
-                                           face[8:96, 8:96], face[4:92, 4:92]]
-                                for i in range(len(patches)):
-                                    cv2.imwrite(output_dir+str(label)+'/'+input_file[:-4]+str(i)+'.'+itype,patches[i])
 
-
-def get_data(input_dir):
+def get_data(input_dir, num_classes):
     """ Gets the data from a directory of images organised by classification.
     :param input_dir: The directory of images.
     :type input_dir: str
     :return: A list of images and labels.
     :rtype: list of tuples each containing an image and a int
     """
-    data = []
-    for label in os.listdir(input_dir):
-        for image_file in os.listdir(input_dir + label):
-            labels = numpy.zeros(8)
+    data, label = [], 0
+    for folder in sorted(os.listdir(input_dir)):
+        for image_file in os.listdir(input_dir + folder):
+            labels = numpy.zeros(num_classes)
             labels[int(label)] = 1
-            data.append((cv2.imread(input_dir + label + '/' + image_file, cv2.IMREAD_GRAYSCALE), labels))
+            data.append((cv2.imread(input_dir + folder + '/' + image_file, cv2.IMREAD_GRAYSCALE), labels))
     return data
 
 
--- main.py
+++ main.py
@@ -8,20 +8,20 @@ def main():
     if len(sys.argv) >= 2:
         mode = sys.argv[1]
         if mode == 'train':
-            if len(sys.argv) > 4:
+            if len(sys.argv) > 5:
                 start = time.clock()
-                faces = builddata.get_data(sys.argv[2])
-                training_data, testing_data = emotionclassifier.divide_data(faces)
+                faces = builddata.get_data(sys.argv[2], int(sys.argv[5]))
+                training_data, testing_data = emotionclassifier.divide_data(faces, 0.3)
                 print 'number of training examples = ' + str(len(training_data))
                 print 'number of testing examples  = ' + str(len(testing_data)) + '\n'
 
-                classifier = emotionclassifier.EmotionClassifier(8, sys.argv[3])
+                classifier = emotionclassifier.EmotionClassifier(int(sys.argv[5]), sys.argv[3])
                 accuracy = classifier.train(training_data, testing_data, int(sys.argv[4]))
                 end = time.clock()
                 print 'Testing Accuracy: ' + '{:.9f}'.format(accuracy)
                 print 'Training Time: ' + '{:.2f}'.format(end - start) + 's'
             else:
-                print 'Please add \'Image Dir\' \'Session Save Path\' \'Number of Epochs\''
+                print 'Please add \'Image Dir\' \'Session Save Path\' \'Number of Epochs\' \'Number of Classes\''
 
         elif mode == 'classify':
             if len(sys.argv) > 3:
